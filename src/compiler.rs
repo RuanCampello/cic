c3e56a9431be49b1d3a773aac9cc1cf6c5d54674
ruancampello <ruancampello999@gmail.com>
2026-09-16 16:37:07 -0300

    test: add some basic testing and correct typo

--- src/compiler.rs
+++ src/compiler.rs
@@ -6,9 +6,34 @@ pub mod resources {
 
 pub fn compile(src: &str) -> String {
     let constant = parse_integer_const(src);
-    resources::TEMPLATE.replace(resources::MARKER, &format!("  mov ${constant}, &rax"))
+    resources::TEMPLATE.replace(resources::MARKER, &format!("  mov ${constant}, %rax"))
 }
 
 fn parse_integer_const(src: &str) -> u64 {
     src.trim().parse::<u64>().expect("a valid constant")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn a_valid_constant() {
+        let output = compile("42");
+        assert!(output.contains("mov $42, %rax"));
+        assert!(!output.contains(resources::MARKER));
+    }
+
+    #[test]
+    fn removes_whitespaces() {
+        let output = compile("     7\n");
+        assert!(output.contains("mov $7, %rax"));
+        assert!(!output.contains(resources::MARKER));
+    }
+
+    #[test]
+    #[should_panic(expected = "a valid constant")]
+    fn invalid_constant() {
+        compile("abc");
+    }
 }
