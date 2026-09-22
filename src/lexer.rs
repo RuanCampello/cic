bb3c7584072d676a1d31041f7be8482119c820a1
ruancampello <ruancampello999@gmail.com>
2026-09-22 17:33:34 -0300

    feat: add primitives for span

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,8 @@
+/// A single token produced by the lexer
+type Token<'src> = Spanned<TokenKind<'src>>;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
-enum Token<'src> {
+enum TokenKind<'src> {
     Integer(u64),
     Punct(Punct),
     Eof,
@@ -8,6 +11,20 @@ enum Token<'src> {
     /// too many times in rust so I'll just put this here now in case we need in the future
     #[doc(hidden)]
     _M(std::marker::PhantomData<&'src ()>),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Spanned<K> {
+    kind: K,
+    span: Span,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+/// A byte range `[start, end)` in the source used to
+/// indicate errors on the respective file
+struct Span {
+    start: u32,
+    end: u32,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -19,4 +36,28 @@ enum Punct {
 
     OpenParen,  // (
     CloseParen, // )
+}
+
+impl Span {
+    pub fn new(start: u32, end: u32) -> Self {
+        assert!(start <= end, "well, shit happens: {start} past its {end}");
+        Self { start, end }
+    }
+
+    pub fn merge(self, other: Span) -> Span {
+        Span {
+            start: self.start.min(other.start),
+            end: self.end.max(other.end),
+        }
+    }
+}
+
+impl<K> Spanned<K> {
+    pub fn new(kind: K, span: Span) -> Self {
+        Self { kind, span }
+    }
+
+    pub fn map<U>(self, f: impl FnOnce(K) -> U) -> Spanned<U> {
+        Spanned { kind: f(self.kind), span: self.span }
+    }
 }
